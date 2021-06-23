82be8eb4b983cb77dba65fad543838da7e097066
Diego Zabala <diegoa.zabala@alumnos.frm.utn.edu.ar>
2021-06-23 03:36:30 -0300

    puede parar esto ?

--- Interfaz/multijugador.gd
+++ Interfaz/multijugador.gd
@@ -126,14 +126,11 @@ remote func post_inicio_juego():
 remote func juego_perdido():
 	rpc_id(get_tree().get_rpc_sender_id(), "juego_perdido")
 	get_tree().change_scene("res://Interfaz/PantallaDerrota.tscn")
-	if juego != null:
-		juego.queue_free()
-	
+
 remote func juego_ganado():
 	rpc_id(get_tree().get_rpc_sender_id(), "juego_ganado")
 	get_tree().change_scene("res://Interfaz/PantallaVictoria.tscn")
-	if juego != null:
-		juego.queue_free()
+
 	
 remote func juego_listo(id):
 	assert(get_tree().is_network_server())
--- Jugador/PrimeraPersona.gd
+++ Jugador/PrimeraPersona.gd
@@ -96,6 +96,7 @@ func morir():
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			get_tree().set_input_as_handled()
+	Multijugador.juego.queue_free()
 	Multijugador.juego_perdido()
 #	rpc_id(get_tree().get_rpc_sender_id(), "juego_perdido")
 #	get_tree().change_scene("res://Interfaz/PantallaDerrota.tscn")
@@ -104,6 +105,7 @@ func ganar():
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 			get_tree().set_input_as_handled()
+	Multijugador.juego.queue_free()
 	Multijugador.juego_ganado()
 #	rpc_id(get_tree().get_rpc_sender_id(), "juego_ganado")
 #	get_tree().change_scene("res://Interfaz/PantallaVictoria.tscn")
